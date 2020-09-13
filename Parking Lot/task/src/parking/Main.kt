3b1ecad4f07b4dade30d998a2c13cfa53d06c98f
Joe Levey <josephfalevey@gmail.com>
2020-09-13 19:04:14 +1000

    complete stage 4

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -7,13 +7,12 @@ var exit = false
 fun main() {
     while (!exit) {
         val command = input.nextLine().split(' ')
-        when {
-            command[0] == "exit" -> exit = true
-            command[0] == "leave" && command[1].toIntOrNull() in 1..CarPark.spots.size -> {
-                CarPark.leave(command[1].toInt())
-            }
-            command.size != 3 -> println("Invalid Request")
-            command[0] == "park" -> Car(command[1], command[2]).park()
+        when (command[0]) {
+            "exit" -> exit = true
+            "status" -> CarPark.status()
+            "create" -> command[1].toIntOrNull()?.let { CarPark.create(it) }
+            "leave"  -> CarPark.leave(command[1].toInt())
+            "park" -> Car(command[1], command[2]).park()
         }
     }
 
@@ -22,6 +21,7 @@ fun main() {
 
 class Car(val registration: String, val color: String) {
     fun park() {
+        if (CarPark.spots.isEmpty()) return println("Sorry, a parking lot has not been created.")
         val spot = CarPark.spots.indexOfFirst { it == null }
         if (spot == -1) return println("Sorry, the parking lot is full.")
         CarPark.spots[spot] = this
@@ -30,11 +30,26 @@ class Car(val registration: String, val color: String) {
 }
 
 object CarPark {
-    val spots: Array<Car?> = Array(20) {null}
+    var spots: Array<Car?> = Array(0) {null}
+    fun create(size: Int) {
+        spots = Array(size) {null}
+        println("Created a parking lot with $size spots.")
+    }
     fun leave(index: Int) {
+        if (spots.isEmpty()) return println("Sorry, a parking lot has not been created.")
+        if (index !in 1..spots.size) return println("Sorry, there is no spot $index.")
         if (spots[index - 1] == null) return println("There is no car in spot $index.")
         spots[index - 1] = null
         println("Spot $index is free.")
+    }
+    fun status() {
+        if (spots.isEmpty()) return println("Sorry, a parking lot has not been created.")
+        if (spots.all { it == null }) return println("Parking lot is empty.")
+        for (i in spots.indices) {
+            if (spots[i] != null) {
+                println("${i + 1} ${spots[i]?.registration} ${spots[i]?.color}")
+            }
+        }
     }
 
 }
